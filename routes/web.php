c660834406f8aa634ac69fe3bfdee6d2afeeb3e9
jezlynnn <jezlynwy@gmail.com>
2024-10-18 08:50:03 +0800

    Added route for login page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('user_signup');
+});
+
+Route::get('/login', function () {
+    return view('user_login');
 });
